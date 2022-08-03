90a9d564aba0a592766a8d85f38a34a266dbafcb
unknown <jshnvirk11@gmail.com>
2022-08-03 10:57:52 +0530

    remove default comments

--- routes/web.php
+++ routes/web.php
@@ -7,16 +7,6 @@ use App\Http\Middleware\UserAuthentication;
 use App\Http\Controllers\StripeController;
 use App\Http\Controllers\PayPalController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('thank', function () {
     return view('thank');
